e6102c091501d130ffed3a5d5a8b4e9302fbcb42
BiaXThePanda <biaxgamestudios@gmail.com>
2022-02-19 13:25:21 +0300

    lineManager

--- SupaPowe/Assets/Scripts/LineManager.cs
+++ SupaPowe/Assets/Scripts/LineManager.cs
@@ -16,6 +16,14 @@ public class LineManager : MonoBehaviour
     public List<Vector2> fingerPositions;
     public float minDistance;
 
+    public float maxDistance;
+
+    public float distanceTraveled;
+    public int maxDrawing;
+    public int drawnLine;
+
+    public bool isDrawing;
+
     private void Start()
     {
         
@@ -25,16 +33,33 @@ public class LineManager : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+
             CreateLine();
 
 
         }
         if (Input.GetMouseButton(0))
         {
+            if(distanceTraveled < maxDistance && drawnLine < maxDrawing) 
+            {
             Vector2 tempFingerPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             if (Vector2.Distance(tempFingerPos,fingerPositions[fingerPositions.Count -1]) > minDistance)
             {
+                distanceTraveled += Vector2.Distance(tempFingerPos, fingerPositions[fingerPositions.Count - 1]);
+                Debug.Log(Vector2.Distance(tempFingerPos, fingerPositions[fingerPositions.Count - 1]));
                 UpdateLine(tempFingerPos);
+                
+
+            }
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (isDrawing)
+            {
+                isDrawing = false;
+                drawnLine++;
+                distanceTraveled = 0;
             }
         }
 
@@ -43,19 +68,25 @@ public class LineManager : MonoBehaviour
 
     void CreateLine()
     {
-        currentLine = Instantiate(linePrefab, Vector3.zero, Quaternion.identity);
-        lineRenderer = currentLine.GetComponent<LineRenderer>();
-        edgeCollider = currentLine.GetComponent<EdgeCollider2D>();
-        fingerPositions.Clear();
-        fingerPositions.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-        fingerPositions.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-        lineRenderer.SetPosition(0, fingerPositions[0]);
-        lineRenderer.SetPosition(1, fingerPositions[1]);
-        edgeCollider.points = fingerPositions.ToArray();
+        if(drawnLine < maxDrawing)
+        {
+            isDrawing = true;
+            currentLine = Instantiate(linePrefab, Vector3.zero, Quaternion.identity);
+            lineRenderer = currentLine.GetComponent<LineRenderer>();
+            edgeCollider = currentLine.GetComponent<EdgeCollider2D>();
+            fingerPositions.Clear();
+            fingerPositions.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            fingerPositions.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            lineRenderer.SetPosition(0, fingerPositions[0]);
+            lineRenderer.SetPosition(1, fingerPositions[1]);
+            edgeCollider.points = fingerPositions.ToArray();
+        }
+        
     }
 
     void UpdateLine(Vector2 newFingerPos)
     {
+        
         fingerPositions.Add(newFingerPos);
         lineRenderer.positionCount++;
         lineRenderer.SetPosition(lineRenderer.positionCount-1,newFingerPos);
