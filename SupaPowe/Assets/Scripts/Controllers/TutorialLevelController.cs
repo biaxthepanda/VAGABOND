cc4930c491ffb5d0a4925814d96f2a384b2aa91c
İ. Çağkan Çağlayanel <cagkancaglayanel@gmail.com>
2022-02-19 22:53:20 +0300

    Tutorial level updated

--- SupaPowe/Assets/Scripts/Controllers/TutorialLevelController.cs
+++ SupaPowe/Assets/Scripts/Controllers/TutorialLevelController.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using DG.Tweening;
 using UnityEngine;
 
 public class TutorialLevelController : Singleton<TutorialLevelController>
@@ -28,7 +30,12 @@ public class TutorialLevelController : Singleton<TutorialLevelController>
     [SerializeField]
     private List<LevelData> _levelData;
 
-    private int _currentLevel = 0;
+    private bool _isTutorialPlaying = false;
+    
+    private bool _isLeftClickDone = false;
+    private bool _isRightClickDone = false;
+    
+    private GameObject _tutorialMouse = null;
 
     private void OnEnable()
     {
@@ -42,14 +49,33 @@ public class TutorialLevelController : Singleton<TutorialLevelController>
 
     private void NewLevel(GameManager.GameState state)
     {
-        if (state != GameManager.GameState.Defending) return;
+        if (state != GameManager.GameState.Attacking) return;
         if (!IsTutorialLevel(LevelManager.Instance.CurrentLevel)) return;
         
         // EnemyController isTutorial = true
             
         StopAllCoroutines();
 
-        StartCoroutine(TutorialStart(_levelData[_currentLevel]));
+        StartCoroutine(TutorialStart(_levelData[LevelManager.Instance.CurrentLevel]));
+    }
+
+    private void Update()
+    {
+        if (!_isTutorialPlaying) return;
+
+        bool leftClick = Input.GetMouseButtonDown(0);
+        bool rightClick = Input.GetMouseButtonUp(1);
+
+        if (leftClick || rightClick)
+        {
+            _isTutorialPlaying = false;
+        }
+
+        if (rightClick)
+            _isRightClickDone = true;
+        if (leftClick)
+            _isLeftClickDone = true;
+
     }
 
     private bool IsTutorialLevel(int level)
@@ -64,13 +90,68 @@ public class TutorialLevelController : Singleton<TutorialLevelController>
     }
 
     IEnumerator TutorialStart(LevelData levelData)
-    { 
-        yield break;   
+    {
+        if (_isTutorialPlaying) yield break;
         
         
+        var startPos = levelData.tutorialStartLocation.position;
+        var endPos = levelData.tutorialEndLocation.position;
+        
+        _isTutorialPlaying = true;
+        _isLeftClickDone = false;
+        _isRightClickDone = false;
+
+        bool isLevelLeftHand = levelData.tutorialType == LevelData.TutorialType.LeftHand;
         
         
+        for (int i = 0; i < 3; i++)
+        {        
+            if (isLevelLeftHand)
+            {
+                if (_isLeftClickDone) break;
+                _tutorialMouse = Instantiate(_leftClickPrefab, startPos, Quaternion.identity);
+
+            } else
+            {
+                if (_isRightClickDone) break;
+                _tutorialMouse = Instantiate(_rightClickPrefab, startPos, Quaternion.identity);
+            }
+            
+            yield return new WaitForSeconds(0.3f);
+            
+            if (_tutorialMouse != null)
+            {
+                _tutorialMouse.transform.DOMove(endPos, 2f);
+
+                Destroy(_tutorialMouse, 2.1f);
+            }
+
+            yield return new WaitForSeconds(.5f);
+            
+            if (isLevelLeftHand)
+                if (_isLeftClickDone) break;
+                else
+                if (_isRightClickDone) break;
+            
+            yield return new WaitForSeconds(.5f);
+            
+            if (isLevelLeftHand)
+                if (_isLeftClickDone) break;
+                else
+                if (_isRightClickDone) break;
+            
+            yield return new WaitForSeconds(1f);
+        }
         
         
+        if (_tutorialMouse != null)
+        {
+            _tutorialMouse.transform.DOKill();
+            Destroy(_tutorialMouse);
+            _tutorialMouse = null;
+        }
+        
+
+        _isTutorialPlaying = false;
     }
 }
